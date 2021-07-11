38bbefe5ec40413463cc32214ed709269aa73e83
Vladimir <vrmy87@yandex.ru>
2021-07-11 18:56:33 +0700

    changed setting for static

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -105,4 +105,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' # префикс для url
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/') # папка, в которой будет лежать статика
