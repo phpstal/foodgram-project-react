87a205d59f8b41af9a4977ff51541684030e8946
Vladimir <vrmy87@yandex.ru>
2021-07-28 21:54:10 +0700

    fixed some errors 1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-v=gmh7^-2)*4-nvp)2^+zny&yqjzkyb@xjh)2fo@q+y(z$ffjv
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '178.154.222.72', 'localhost',]
+ALLOWED_HOSTS = ['127.0.0.1', '178.154.222.72', 'localhost', 'backend', 'web']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
